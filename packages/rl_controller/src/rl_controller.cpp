dd91beeee141ac72179acad8f8870841b78fee2d
denys <ddatsko010101@gmail.com>
2023-04-01 02:29:01 +0200

    Added module for goals tracking and setting. Updated tmux session file

--- packages/rl_controller/src/rl_controller.cpp
+++ packages/rl_controller/src/rl_controller.cpp
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 
 #include <nav_msgs/Odometry.h>
+#include <mrs_msgs/ReferenceSrv.h>
 
 #include <random>
 
@@ -14,7 +15,7 @@
 #include <geometry_msgs/PoseStamped.h>
 
 #include <dynamic_reconfigure/server.h>
-#include <controller_module/controller_paramsConfig.h>
+#include <rl_controller/controller_paramsConfig.h>
 
 #include <eigen3/Eigen/Eigen>
 
@@ -24,48 +25,15 @@
 
 //}
 
-namespace controller_module {
+namespace rl_controller {
 
-/* class ControllerModule //{ */
 
-    class ControllerModule : public mrs_uav_managers::Controller {
+    class RLController : public mrs_uav_managers::Controller {
 
-        class Goal {
-        public:
-            Eigen::Vector3d position;
-            Eigen::Vector3d direction;
-            double tolerance;
-
-            Goal(const Eigen::Vector3d &position, const Eigen::Vector3d &direction, double tolerance) :
-                    position{position}, direction{direction}, tolerance{tolerance} {};
-
-            [[nodiscard]] bool reached(const Eigen::Vector3d &prev_pos, const Eigen::Vector3d &current_pos) const {
-                Eigen::Vector3d v = current_pos - prev_pos;  // step movement vector
-                Eigen::Vector3d n = direction;           // goal direction vector
-                double vTn = v.dot(n);          // dot product of the vector
-
-                // either runs in parallel or passed in other direction
-                if (vTn <= 0.0) return false;
-
-                Eigen::Vector3d a = prev_pos - position;  // vector from initial position to goal position
-                // how many times the current would be taken to pass the goal (can be
-                // negative)
-                double t = -(a.dot(n) / vTn);
-                if (t >= 0 && t <= 1) {  // only these values mean agent passed by the goal in
-                    // the current time-step
-                    Eigen::Vector3d intersect = a + t * v;  // intersection point
-                    if (intersect.norm() <= tolerance)  // is the intersection point within tolerance
-                        return true;
-
-                }
-                return false;
-            }
-
-        };
-
+        ros::ServiceServer goal_service_server;
 
     public:
-        ~ControllerModule() override = default;
+        ~RLController() override = default;
 
         void initialize(const ros::NodeHandle &parent_nh, std::string name, std::string name_space,
                         double uav_mass,
@@ -92,25 +60,21 @@ namespace controller_module {
         bool is_active_ = false;
 
         std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers_;
-
-        std::vector<Goal> m_goals_to_reach;
-
         // Just default values to not leave fields uninitialized. All will be replaced in initialize() function
         double _uav_mass_ = 3.5;
         double hover_thrust_ = 0;
         double m_uav_mass = 3.5;
-        size_t m_current_goal_index = 0;
-//        std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d>> points_to_visit;
 
+        Eigen::Vector3d m_current_goal;
 
         // | --------------- dynamic reconfigure server --------------- |
 
         boost::recursive_mutex mutex_drs_;
-        typedef controller_module::controller_paramsConfig DrsParams_t;
+        typedef rl_controller::controller_paramsConfig DrsParams_t;
         typedef dynamic_reconfigure::Server<DrsParams_t> Drs_t;
         boost::shared_ptr<Drs_t> drs_;
 
-        void callbackDrs(controller_module::controller_paramsConfig &params, uint32_t level);
+        void callbackDrs(rl_controller::controller_paramsConfig &params, uint32_t level);
 
         DrsParams_t params_;
         std::mutex mutex_params_;
@@ -119,7 +83,10 @@ namespace controller_module {
         std::mutex m_uav_state_mutex;
         std::mutex m_model_mutex;
         torch::jit::script::Module m_policy_module;
-        Eigen::Vector3d m_prev_pos;
+
+        ros::ServiceServer m_set_goal_service_server;
+
+        bool callback_set_goal(mrs_msgs::ReferenceSrv::Request &req, mrs_msgs::ReferenceSrv::Response &res);
 
 //        static bool goal_passed(in)
     };
@@ -132,10 +99,10 @@ namespace controller_module {
 
 /* initialize() //{ */
 
-    void ControllerModule::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const std::string name,
-                                      const std::string name_space,
-                                      const double uav_mass,
-                                      std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers) {
+    void RLController::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const std::string name,
+                                  const std::string name_space,
+                                  const double uav_mass,
+                                  std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers) {
 
         ros::NodeHandle nh_(parent_nh, name_space);
 
@@ -146,7 +113,7 @@ namespace controller_module {
 
         // | ------------------- loading parameters ------------------- |
 
-        mrs_lib::ParamLoader param_loader(nh_, "ControllerModule");
+        mrs_lib::ParamLoader param_loader(nh_, "RLController");
 
         /* TODO: move this to parameters inside a normal namespace. Current problem is that
         if working with ParamLoader, a custom prefix should be set or control_manager.launch file should be changed */
@@ -155,27 +122,20 @@ namespace controller_module {
         // TODO: Read this from some parameter
         m_uav_mass = 3.5;
 
-        // TODO: read this from some config file of ROS parameter too
-        m_current_goal_index = 0;
-        m_prev_pos = {0, 0, 0};
-        m_goals_to_reach.emplace_back(Eigen::Vector3d{2.0, 2.0, 2.0}, Eigen::Vector3d{1.0, 1.0, 1.0}, 0.5);
-        m_goals_to_reach.emplace_back(Eigen::Vector3d{3.0, 3.0, 4.0}, Eigen::Vector3d{0.0, 0.0, 1.0}, 0.5);
-        m_goals_to_reach.emplace_back(Eigen::Vector3d{0.0, 0.0, 5.0}, Eigen::Vector3d{-1.0, -1.0, 0.0}, 0.5);
-        m_goals_to_reach.emplace_back(Eigen::Vector3d{-2.0, 0.0, 3.0}, Eigen::Vector3d{0.0, 0.0, -1.0}, 0.5);
-
-
         if (!param_loader.loadedSuccessfully()) {
-            ROS_ERROR("[ControllerModule]: Could not load all parameters!");
+            ROS_ERROR("[RLController]: Could not load all parameters!");
             ros::shutdown();
         }
+
+        m_set_goal_service_server = nh_.advertiseService("set_goal", &RLController::callback_set_goal, this);
 
         try {
             m_policy_module = torch::jit::load(policy_filename);
         } catch (const c10::Error &e) {
-            ROS_ERROR_STREAM("[ControllerModule]: failed to load jit script! Error: " << e.what());
+            ROS_ERROR_STREAM("[RLController]: failed to load jit script! Error: " << e.what());
             ros::shutdown();
         }
-        ROS_INFO("[ControllerModule]: policy file loaded successfully!");
+        ROS_INFO("[RLController]: policy file loaded successfully!");
 
 
         // | ----------- calculate the default hover thrust ----------- |
@@ -185,25 +145,38 @@ namespace controller_module {
         // | --------------------------- drs -------------------------- |
 
         drs_.reset(new Drs_t(mutex_drs_, nh_));
-        Drs_t::CallbackType f = boost::bind(&ControllerModule::callbackDrs, this, _1, _2);
+        Drs_t::CallbackType f = boost::bind(&RLController::callbackDrs, this, _1, _2);
         drs_->setCallback(f);
 
         // | ----------------------- finish init ---------------------- |
 
-        ROS_INFO("[ControllerModule]: initialized");
+        ROS_INFO("[RLController]: initialized");
 
         is_initialized_ = true;
     }
 
 //}
 
+    bool RLController::callback_set_goal(mrs_msgs::ReferenceSrv::Request &req, mrs_msgs::ReferenceSrv::Response &res) {
+        m_current_goal.x() = req.reference.position.x;
+        m_current_goal.y() = req.reference.position.y;
+        m_current_goal.z() = req.reference.position.z;
+
+        ROS_INFO_STREAM("[RLController]: New goal set: \n" << m_current_goal);
+
+        res.message = "New goal set";
+        res.success = true;
+        return true;
+    }
+
+
 /* activate() //{ */
 
-    bool ControllerModule::activate(const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd) {
+    bool RLController::activate(const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd) {
 
         if (last_attitude_cmd == mrs_msgs::AttitudeCommand::Ptr()) {
 
-            ROS_WARN("[ControllerModule]: activated without getting the last controller's command");
+            ROS_WARN("[RLController]: activated without getting the last controller's command");
 
             return false;
         }
@@ -217,11 +190,11 @@ namespace controller_module {
 
 /* deactivate() //{ */
 
-    void ControllerModule::deactivate(void) {
+    void RLController::deactivate(void) {
 
         is_active_ = false;
 
-        ROS_INFO("[ControllerModule]: deactivated");
+        ROS_INFO("[RLController]: deactivated");
     }
 
 //}
@@ -229,10 +202,10 @@ namespace controller_module {
 /* update() //{ */
 
     const mrs_msgs::AttitudeCommand::ConstPtr
-    ControllerModule::update([[maybe_unused]] const mrs_msgs::UavState::ConstPtr &uav_state_p,
-                             [[maybe_unused]] const mrs_msgs::PositionCommand::ConstPtr &control_reference) {
+    RLController::update([[maybe_unused]] const mrs_msgs::UavState::ConstPtr &uav_state_p,
+                         [[maybe_unused]] const mrs_msgs::PositionCommand::ConstPtr &control_reference) {
 
-//        ROS_INFO_ONCE("[ControllerModule]: update()");
+//        ROS_INFO_ONCE("[RLController]: update()");
         if (!is_active_) {
             return mrs_msgs::AttitudeCommand::ConstPtr();
         }
@@ -244,17 +217,6 @@ namespace controller_module {
             uav_state = *uav_state_p;
         }
 
-        // Check if the UAV crossed the next waypoint and if it has, update the waypoint to the next one
-        Eigen::Vector3d current_position{uav_state.pose.position.x, uav_state.pose.position.y,
-                                         uav_state.pose.position.z};
-        if (m_goals_to_reach[m_current_goal_index].reached(m_prev_pos, current_position)) {
-            ROS_INFO_STREAM(
-                    "[ControllerModule]: Reached the waypoint: " << m_goals_to_reach[m_current_goal_index].position);
-            m_current_goal_index = std::min(m_current_goal_index + 1, m_goals_to_reach.size() - 1);
-            ROS_INFO_STREAM(
-                    "[ControllerModule]: Reached the waypoint. Next one to follow is number " << m_current_goal_index);
-        }
-        m_prev_pos = current_position;
 
         // Get rotation matrix from orientation quaternion
         Eigen::Quaterniond orientation_q;
@@ -264,7 +226,7 @@ namespace controller_module {
         orientation_q.w() = uav_state.pose.orientation.w;
         Eigen::Matrix3d R = orientation_q.normalized().toRotationMatrix();
 
-        // Fill in model inputs. TODO: check if these are correct values
+        // Fill in model inputs
         float model_input[18];
         model_input[0] = static_cast<float>(uav_state.pose.position.x);
         model_input[1] = static_cast<float>(uav_state.pose.position.y);
@@ -280,12 +242,9 @@ namespace controller_module {
             model_input[i] = static_cast<float>(R_data[i - 6]);
         }
 
-        model_input[15] = static_cast<float>(m_goals_to_reach[m_current_goal_index].position.x() -
-                                             uav_state.pose.position.x);
-        model_input[16] = static_cast<float>(m_goals_to_reach[m_current_goal_index].position.y() -
-                                             uav_state.pose.position.y);
-        model_input[17] = static_cast<float>(m_goals_to_reach[m_current_goal_index].position.z() -
-                                             uav_state.pose.position.z);
+        model_input[15] = static_cast<float>(m_current_goal.x() - uav_state.pose.position.x);
+        model_input[16] = static_cast<float>(m_current_goal.y() - uav_state.pose.position.y);
+        model_input[17] = static_cast<float>(m_current_goal.z() - uav_state.pose.position.z);
 
         torch::Tensor input_tensor = torch::from_blob(model_input, {1, 18});
         torch::IValue input_ivalue{input_tensor};
@@ -312,19 +271,19 @@ namespace controller_module {
         output_command->attitude_rate.y = model_output[0][2].item<float>();
         output_command->attitude_rate.z = model_output[0][3].item<float>();
 
-        ROS_DEBUG_STREAM("[ControllerModule]: UAV position: \n" << uav_state.pose.position << "\n" <<
-                                                                "UAV rotation: " << R << "\n" <<
-                                                                "Current reference point: "
-                                                                << m_goals_to_reach[m_current_goal_index].position <<
-                                                                "Desired body rates: "
-                                                                << model_output[0][1].item<float>() << ", "
-                                                                << model_output[0][2].item<float>() << ", "
-                                                                << model_output[0][3].item<float>() << "\n" <<
-                                                                "Desired thrust: " << output_command->thrust << "\n\n");
+        ROS_DEBUG_STREAM("[RLController]: UAV position: \n" << uav_state.pose.position << "\n" <<
+                                                            "UAV rotation: " << R << "\n" <<
+                                                            "Current reference point: "
+                                                            << m_current_goal <<
+                                                            "Desired body rates: "
+                                                            << model_output[0][1].item<float>() << ", "
+                                                            << model_output[0][2].item<float>() << ", "
+                                                            << model_output[0][3].item<float>() << "\n" <<
+                                                            "Desired thrust: " << output_command->thrust << "\n\n");
 
         output_command->controller_enforcing_constraints = false;
 
-        output_command->controller = "ControllerModule";
+        output_command->controller = "RLController";
 
         return output_command;
 
@@ -337,18 +296,18 @@ namespace controller_module {
 /* //{ callbackDrs() */
 
     void
-    ControllerModule::callbackDrs(controller_module::controller_paramsConfig &params, [[maybe_unused]] uint32_t level) {
+    RLController::callbackDrs(rl_controller::controller_paramsConfig &params, [[maybe_unused]] uint32_t level) {
 
         mrs_lib::set_mutexed(mutex_params_, params, params_);
 
-        ROS_INFO("[ControllerModule]: DRS updated");
+        ROS_INFO("[RLController]: DRS updated");
     }
 
 //}
 
 /* getStatus() //{ */
 
-    const mrs_msgs::ControllerStatus ControllerModule::getStatus() {
+    const mrs_msgs::ControllerStatus RLController::getStatus() {
 
         mrs_msgs::ControllerStatus controller_status;
 
@@ -361,22 +320,22 @@ namespace controller_module {
 
 /* switchOdometrySource() //{ */
 
-    void ControllerModule::switchOdometrySource([[maybe_unused]] const mrs_msgs::UavState::ConstPtr &new_uav_state) {
+    void RLController::switchOdometrySource([[maybe_unused]] const mrs_msgs::UavState::ConstPtr &new_uav_state) {
     }
 
 //}
 
 /* resetDisturbanceEstimators() //{ */
 
-    void ControllerModule::resetDisturbanceEstimators(void) {
+    void RLController::resetDisturbanceEstimators(void) {
     }
 
 //}
 
 /* setConstraints() //{ */
 
-    const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr ControllerModule::setConstraints([
-                                                                                              [maybe_unused]] const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &constraints) {
+    const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr RLController::setConstraints([
+                                                                                          [maybe_unused]] const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &constraints) {
 
         return mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr(new mrs_msgs::DynamicsConstraintsSrvResponse());
     }
@@ -387,4 +346,4 @@ namespace controller_module {
 
 #include <pluginlib/class_list_macros.h>
 
-PLUGINLIB_EXPORT_CLASS(controller_module::ControllerModule, mrs_uav_managers::Controller)
+PLUGINLIB_EXPORT_CLASS(rl_controller::RLController, mrs_uav_managers::Controller)
---
+++ packages/rl_goals_checker/src/RLGoalsChecker.cpp
@@ -0,0 +1,161 @@
+#include <RLGoalsChecker.h>
+#include <pluginlib/class_list_macros.h>
+#include <mrs_lib/param_loader.h>
+#include <mrs_lib/transformer.h>
+#include <yaml-cpp/yaml.h>
+
+
+namespace rl_goals_checker {
+
+    std::vector<Goal> read_yaml_config_file(const std::string &filename) {
+        YAML::Node root_node;
+        try {
+            root_node = YAML::LoadFile(filename);
+        } catch (const YAML::Exception &e) {
+            ROS_ERROR_STREAM("[RLGoalsChecker]: Could not read environment config file: " << filename);
+            return {};
+        }
+        if (root_node["environment_configuration"] && root_node["environment_configuration"]["goals"]) {
+            std::vector<Goal> read_goals;
+            auto goals = root_node["environment_configuration"]["goals"];
+            for (auto &&goal: goals) {
+                try {
+                    Goal g;
+                    g.tolerance = goal["tolerance"].as<float>();
+                    g.position.x() = goal["position"][0].as<float>();
+                    g.position.y() = goal["position"][1].as<float>();
+                    g.position.z() = goal["position"][2].as<float>();
+
+                    g.direction.x() = goal["direction"][0].as<float>();
+                    g.direction.y() = goal["direction"][1].as<float>();
+                    g.direction.z() = goal["direction"][2].as<float>();
+
+                    read_goals.push_back(g);
+
+                } catch (const std::runtime_error &e) {
+                    ROS_ERROR_STREAM("[RLGoalsChecker]: Error while reading configuration file: " << e.what());
+                    return {};
+                }
+            }
+            return read_goals;
+        } else {
+            ROS_ERROR_STREAM("[RLGoalsChecker]: Error. No environment_configuration/goals in file " << filename);
+            return {};
+        }
+        return {};
+    }
+
+
+    bool Goal::reached(const Eigen::Vector3d &prev_pos, const Eigen::Vector3d &current_pos) const {
+        Eigen::Vector3d v = current_pos - prev_pos;  // step movement vector
+        Eigen::Vector3d n = direction;           // goal direction vector
+        double vTn = v.dot(n);          // dot product of the vector
+
+        // either runs in parallel or passed in other direction
+        if (vTn <= 0.0) return false;
+
+        Eigen::Vector3d a = prev_pos - position;  // vector from initial position to goal position
+        // how many times the current would be taken to pass the goal (can be
+        // negative)
+        double t = -(a.dot(n) / vTn);
+        if (t >= 0 && t <= 1) {  // only these values mean agent passed by the goal in
+            // the current time-step
+            Eigen::Vector3d intersect = a + t * v;  // intersection point
+            if (intersect.norm() <= tolerance)  // is the intersection point within tolerance
+                return true;
+
+        }
+        return false;
+    }
+
+    void RLGoalsChecker::onInit() {
+        /* obtain node handle */
+        ros::NodeHandle nh = nodelet::Nodelet::getMTPrivateNodeHandle();
+
+        /* waits for the ROS to publish clock */
+        ros::Time::waitForValid();
+
+
+        std::string environment_config_filename;
+        // | ------------------- load ros parameters ------------------ |
+        mrs_lib::ParamLoader pl(nh, "RLGoalsChecker");
+
+        pl.loadParam("UAV_NAME", m_uav_name);
+        pl.loadParam("environment_configuration_file", environment_config_filename);
+        pl.loadParam("goal_frame_id", m_goal_frame_id);
+
+        if (!pl.loadedSuccessfully()) {
+            ROS_ERROR("[RLGoalsChecker]: failed to load non-optional parameters!");
+            ros::shutdown();
+        } else {
+            ROS_INFO_ONCE("[RLGoalsChecker]: loaded parameters");
+        }
+
+        // Read environment configuration
+        m_goals_to_visit = read_yaml_config_file(environment_config_filename);
+        if (m_goals_to_visit.empty()) {
+            ROS_ERROR_STREAM("[RLGoalsChecker]: Error. No goals read from file " << environment_config_filename <<
+                                                                                 ". Nodelet will not post any goals");
+            ros::shutdown();
+
+        } else {
+            ROS_INFO_STREAM("[RLGoalsChecker]: Successfully read " << m_goals_to_visit.size() << " goals from file");
+        }
+
+        m_set_goal_service_client = nh.serviceClient<mrs_msgs::ReferenceSrv>(
+                "/" + m_uav_name + "/control_manager/rl_controller/set_goal");
+        ROS_INFO_STREAM("[RLGoalsChecker]: Waiting for existence of service server...");
+        m_set_goal_service_client.waitForExistence();
+        ROS_INFO_STREAM("[RLGoalsChecker]: Service server is present. Continuing");
+        send_current_goal();
+
+        m_odometry_subscriber = nh.subscribe("odometry_in", 10, &RLGoalsChecker::m_odometry_callback,
+                                             this);
+
+        m_transformer = mrs_lib::Transformer("RLGoalsChecker");
+
+        ROS_INFO_ONCE("[RLGoalsChecker]: initialized");
+    }
+
+    void RLGoalsChecker::m_odometry_callback(const nav_msgs::Odometry &odom_msg) {
+        auto odom_position = m_transformer.transformSingle(odom_msg, m_goal_frame_id);
+        if (!odom_position.has_value()) {
+            ROS_ERROR_STREAM("[RLGoalsChecker]: Could not convert odometry to frame " << m_goal_frame_id << ". Skipping measurement");
+            return;
+        }
+
+        Eigen::Vector3d position = {odom_position->pose.pose.position.x,
+                                    odom_position->pose.pose.position.y,
+                                    odom_position->pose.pose.position.z};
+
+        if (!m_previous_position_received) {
+            m_previous_position_received = true;
+            m_previous_position = position;
+            return;
+        }
+
+        if (m_goals_to_visit[m_current_goal].reached(m_previous_position, position)) {
+            ROS_INFO_STREAM("[RLGoalsChecker]: UAV reached goal: \n" << m_goals_to_visit[m_current_goal].position);
+
+            // TODO: change the behaviour when last goal reached. Maybe, change controller to MPC or so
+            m_current_goal = std::min(m_current_goal + 1, m_goals_to_visit.size() - 1);
+
+            // If the goal is reached -- send a new one to the controller
+            send_current_goal();
+        }
+        m_previous_position = position;
+    }
+
+    void RLGoalsChecker::send_current_goal() {
+        mrs_msgs::ReferenceSrv set_goal_request;
+        set_goal_request.request.reference.position.x = m_goals_to_visit[m_current_goal].position.x();
+        set_goal_request.request.reference.position.y = m_goals_to_visit[m_current_goal].position.y();
+        set_goal_request.request.reference.position.z = m_goals_to_visit[m_current_goal].position.z();
+        m_set_goal_service_client.call(set_goal_request);
+    }
+
+
+}  // namespace ro_goals_checker  
+
+/* every nodelet must export its class as nodelet plugin */
+PLUGINLIB_EXPORT_CLASS(rl_goals_checker::RLGoalsChecker, nodelet::Nodelet)
